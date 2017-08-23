c25ce7bba7f61e1e78c3e526d3ff474788aa6809
Alison L. McGuire <alison.l.mcguire@avanade.com>
2017-08-23 10:14:07 -0700

    separting files

---
+++ SimpleAddin.js
@@ -0,0 +1,27 @@
+
+// The initialize function must be run each time a new page is loaded
+(function () {
+Office.initialize = function (reason) {
+    $(document).ready(function () {
+        $("#writeTextButton").click(function (event) {
+            writeText();
+        });
+        //
+    });
+};
+
+// Reads data from current document selection and displays a notification
+function writeText() {
+    Office.context.document.setSelectedDataAsync("OMGGG",
+        function (asyncResult) {
+            var error = asyncResult.error;
+            if (asyncResult.status === "failed") {
+                //show error. Upcoming displayDialog API will help here.
+            }
+            else {
+                //show success.Upcoming displayDialog API will help here.
+            }
+        });
+}
+
+})();
