0d2b33f94f447bc48a21f15d4876dfacb2ecb8d8
Alison L. McGuire <alison.l.mcguire@avanade.com>
2017-08-23 11:53:32 -0700

    buttons!

--- SimpleAddin.js
+++ SimpleAddin.js
@@ -3,25 +3,29 @@
 (function () {
 Office.initialize = function (reason) {
     $(document).ready(function () {
-        $("#writeTextButton").click(function (event) {
-            writeText(text);
+        $("#createNew").click(function (event) {
+            createNew();
         });
         //
     });
 };
 
 // Reads data from current document selection and displays a notification
-function writeText() {
-    Office.context.document.setSelectedDataAsync("Something",
-        function (asyncResult) {
-            var error = asyncResult.error;
-            if (asyncResult.status === "failed") {
-                //show error. Upcoming displayDialog API will help here.
-            }
-            else {
-                //show success.Upcoming displayDialog API will help here.
-            }
-        });
+function createNew() {
+    $("#allComments").classList.add("hidden");
+    $("#createNew").classList.add("hidden");
+    $("#back").classList.remove("hidden");
+
+    // Office.context.document.setSelectedDataAsync("Something",
+    //     function (asyncResult) {
+    //         var error = asyncResult.error;
+    //         if (asyncResult.status === "failed") {
+    //             //show error. Upcoming displayDialog API will help here.
+    //         }
+    //         else {
+    //             //show success.Upcoming displayDialog API will help here.
+    //         }
+    //     });
 }
 
 })();
